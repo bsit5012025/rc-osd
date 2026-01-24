ec47bbd69fbb8fda236c7c249df47b89cd17f00b
“[wilroww]” <wilroww.reosa.bayona@gmail.com>
2026-01-24 16:23:55 +0800

    Added opens and exports org.rocs.osd to fix javaFX issue and change new versions of maven and javaFx in pom.xml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,4 +2,7 @@ module org.rocs.osd {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+
+    opens org.rocs.osd to javafx.fxml; // added opens and exports org.rocs.osd to fix javaFX issue
+    exports org.rocs.osd;
 }
