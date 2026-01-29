8cc996adda484d4f48c6d2cde00c96a109f2fabf
unknown <keithjasper.quevada@gmail.com>
2026-01-29 22:52:15 +0800

    1st commit
    # Conflicts:
    #	src/main/java/org/rocs/osd/controller/login/LoginController.java
    #	src/main/resources/view/login/login.fxml

--- src/main/java/org/rocs/osd/OfficeOfStudentDisciplineApplication.java
+++ src/main/java/org/rocs/osd/OfficeOfStudentDisciplineApplication.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.rocs.osd.controller.login.LoginController;
 
 public class OfficeOfStudentDisciplineApplication extends Application {
     @Override
