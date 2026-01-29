be5bcdbfa1d08c2f0b88e84141ef60236df84574
unknown <keithjasper.quevada@gmail.com>
2026-01-29 22:52:15 +0800

    1st commit

--- src/main/java/org/rocs/osd/OfficeOfStudentDisciplineApplication.java
+++ src/main/java/org/rocs/osd/OfficeOfStudentDisciplineApplication.java
@@ -5,12 +5,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.rocs.osd.controller.login.LoginController;
 
 public class OfficeOfStudentDisciplineApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/login/login.fxml"));
-
         Parent mainLayout = loader.load();
         Scene scene = new Scene(mainLayout);
         stage.setMaximized(true);
--- src/main/java/org/rocs/osd/controller/login/LoginController.java
+++ src/main/java/org/rocs/osd/controller/login/LoginController.java
@@ -1,5 +1,27 @@
 package org.rocs.osd.controller.login;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
 public class LoginController {
 
+    @FXML
+    TextField usernameTField;
+    @FXML
+    TextField passwordTField;
+
+    public void toLogin(ActionEvent event){
+
+        if(usernameTField.getText().isBlank() == false && passwordTField.getText().isBlank()){
+            System.out.println("Enter unsername and password!");
+        }
+        else{
+            System.out.println("Username: " +usernameTField.getText());
+            System.out.println("Password: " +passwordTField.getText());
+        }
+    }
 }
