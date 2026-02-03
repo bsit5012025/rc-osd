648f3e1b83a134498625c6a76688e8aa5a07c93a
unknown <keithjasper.quevada@gmail.com>
2026-02-03 18:51:21 +0800

    remove imports on controller different ticket

--- src/main/java/org/rocs/osd/controller/login/LoginController.java
+++ src/main/java/org/rocs/osd/controller/login/LoginController.java
@@ -1,12 +1,5 @@
 package org.rocs.osd.controller.login;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.control.TextField;
-
-import java.io.IOException;
-
 public class LoginController {
 
 }
