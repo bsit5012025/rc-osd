de8bbce98c12e03db2fe71b47f5746f5f8d9cf19
carlcain <carljustinecain@gmail.com>
2026-02-03 12:38:33 +0800

    added a logic on LoginFacadeImpl

--- src/main/java/org/rocs/osd/data/dao/login/LoginDao.java
+++ src/main/java/org/rocs/osd/data/dao/login/LoginDao.java
@@ -3,7 +3,5 @@ package org.rocs.osd.data.dao.login;
 import org.rocs.osd.model.login.Login;
 
 public interface LoginDao {
-
     public Login findLoginByUsername(String username);
-
 }
--- src/main/java/org/rocs/osd/facade/login/impl/LoginFacadeImpl.java
+++ src/main/java/org/rocs/osd/facade/login/impl/LoginFacadeImpl.java
@@ -1,19 +1,29 @@
 package org.rocs.osd.facade.login.impl;
 
+import org.rocs.osd.data.dao.login.LoginDao;
 import org.rocs.osd.facade.login.LoginFacade;
 import org.rocs.osd.model.login.Login;
 
-public class LoginFacadeImpl implements LoginFacade {
-
+public class LoginFacadeImpl implements LoginFacade
+{
+    private LoginDao loginDao;
 
     @Override
-    public boolean login(String inputUserName, String inputPassword) {
-        return false;
+    public boolean login(String inputUserName, String inputPassword)
+    {
+        Login login = loginDao.findLoginByUsername(inputUserName);
+
+        if(login == null)
+        {
+            return false;
+        }
+
+        return inputPassword.equals(login.getPassword());
     }
 
     @Override
-    public Login returnUserInfo(String inputed_UserName) {
-        return null;
+    public Login returnUserInfo(String inputedUserName)
+    {
+        return  loginDao.findLoginByUsername(inputedUserName);
     }
-
 }
