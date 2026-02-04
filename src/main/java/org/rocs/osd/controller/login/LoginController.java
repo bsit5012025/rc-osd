7fd9fc3937800a48177efdc89add27ef0d6abff3
keithjasperquevada <keithjasper.quevada@gmail.com>
2026-02-04 19:06:09 +0800

    Feature/osda 38 controller login (#13)
    * 1st commit
    
    # Conflicts:
    #	src/main/java/org/rocs/osd/controller/login/LoginController.java
    #	src/main/resources/view/login/login.fxml
    
    * controller for login
    
    * delete auto generated file on .idea
    
    * change declaration of loginfacade to local, and remove SysOut checker
    
    * remove controller import in application
    
    * delete .idea file
    
    * change id name
    
    * rename action event id and class

--- src/main/java/org/rocs/osd/controller/login/LoginController.java
+++ src/main/java/org/rocs/osd/controller/login/LoginController.java
@@ -1,5 +1,57 @@
 package org.rocs.osd.controller.login;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.rocs.osd.data.dao.login.LoginDao;
+import org.rocs.osd.data.dao.login.impl.LoginDaoImpl;
+import org.rocs.osd.facade.login.LoginFacade;
+import org.rocs.osd.facade.login.impl.LoginFacadeImpl;
+
+import java.io.IOException;
+
 public class LoginController {
-    
+
+    @FXML
+    TextField usernameTextField;
+    @FXML
+    PasswordField passwordField;
+
+    public void onLogin(ActionEvent event){
+
+        LoginFacade loginFacade;
+        LoginDao loginDao = new LoginDaoImpl();
+        loginFacade = new LoginFacadeImpl(loginDao);
+
+        boolean loginCheck = loginFacade.login(usernameTextField.getText(),passwordField.getText());
+
+        if(usernameTextField.getText().isBlank() == false && passwordField.getText().isBlank()){
+            System.out.println("Enter unsername and password!");
+            return;
+        }
+        if(loginCheck){
+            loadDashboard(event);
+        }
+        else{
+            System.out.println("Invalid username or password!");
+        }
+    }
+
+    private void loadDashboard(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/view/dashboard/dashboard.fxml"));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        ;
+    }
 }
