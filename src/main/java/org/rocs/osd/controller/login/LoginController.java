d8bc3da7c32a8009f515d4725099e269ee69e7f5
unknown <keithjasper.quevada@gmail.com>
2026-02-04 17:29:37 +0800

    controller for login

--- src/main/java/org/rocs/osd/controller/login/LoginController.java
+++ src/main/java/org/rocs/osd/controller/login/LoginController.java
@@ -1,5 +1,61 @@
 package org.rocs.osd.controller.login;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.rocs.osd.data.dao.login.LoginDao;
+import org.rocs.osd.data.dao.login.impl.LoginDaoImpl;
+import org.rocs.osd.facade.login.LoginFacade;
+import org.rocs.osd.facade.login.impl.LoginFacadeImpl;
+
+import java.io.IOException;
+
 public class LoginController {
-    
+
+    @FXML
+    TextField usernameTField;
+    @FXML
+    PasswordField passwordField;
+
+    private LoginFacade loginFacade;
+
+    public void toLogin(ActionEvent event){
+
+        LoginDao loginDao = new LoginDaoImpl();
+        loginFacade = new LoginFacadeImpl(loginDao);
+
+        boolean loginCheck = loginFacade.login(usernameTField.getText(),passwordField.getText());
+
+        if(usernameTField.getText().isBlank() == false && passwordField.getText().isBlank()){
+            System.out.println("Enter unsername and password!");
+            System.out.println("Username: " +usernameTField.getText());
+            System.out.println("Password: " +passwordField.getText());
+            return;
+        }
+        if(loginCheck){
+            loadDashboard(event);
+        }
+        else{
+            System.out.println("Username: " +usernameTField.getText());
+            System.out.println("Password: " +passwordField.getText());
+        }
+    }
+
+    private void loadDashboard(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/view/dashboard/dashboard.fxml"));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        ;
+    }
 }
