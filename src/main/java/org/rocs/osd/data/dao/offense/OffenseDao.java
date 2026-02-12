84e376999bb8839a052ca9c977823350e2f0faf3
carlcain <carljustinecain@gmail.com>
2026-02-12 09:51:54 +0800

    OSDA-109: added the models that zen created and made a few changes on my DAO for recording violation

--- src/main/java/org/rocs/osd/data/dao/offense/OffenseDao.java
+++ src/main/java/org/rocs/osd/data/dao/offense/OffenseDao.java
@@ -1,12 +1,18 @@
 package org.rocs.osd.data.dao.offense;
 
-import org.rocs.osd.model.student.Student;
+import org.rocs.osd.model.offense.Offense;
+import org.rocs.osd.model.record.Record;
+import org.rocs.osd.model.person.student.Student;
 
 import java.util.ArrayList;
 
 public interface OffenseDao
 {
-    ArrayList<Student> getStudentById(String studentID);
+    Student getStudentById(String studentID);
+    Student getStudentByName(String lastName, String firstName, String middleName);
+    ArrayList<Record> getStudentRecord(String studentID);
+    Offense getStudentOffense(String offenseID);
+
 
     boolean addStudentViolation(String recordID, String enrollmentID, String prefectID,
                              String offenseID, String dateOfViolation, String actionID,
--- src/main/java/org/rocs/osd/data/dao/offense/impl/OffenseDaoImpl.java
+++ src/main/java/org/rocs/osd/data/dao/offense/impl/OffenseDaoImpl.java
@@ -2,57 +2,183 @@ package org.rocs.osd.data.dao.offense.impl;
 
 import org.rocs.osd.data.connection.ConnectionHelper;
 import org.rocs.osd.data.dao.offense.OffenseDao;
-import org.rocs.osd.model.student.Student;
+import org.rocs.osd.model.offense.Offense;
+import org.rocs.osd.model.person.student.Student;
+import org.rocs.osd.model.record.Record;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
 import java.util.ArrayList;
 
 public class OffenseDaoImpl implements OffenseDao
 {
 
     @Override
-    public ArrayList<Student> getStudentById(String StudentId)
+    public Student getStudentById(String StudentId)
     {
-        ArrayList<Student> studentOffenceList = new ArrayList<>();
+        Student student = new Student();
         try (Connection conn = ConnectionHelper.getConnection())
         {
             PreparedStatement statement = conn.prepareStatement(
                         "SELECT " +
-                            "e.studentID, " +
-                            "p.lastName,  " +
-                                "p.firstName, " +
-                            "o.type AS offense_type, " +
-                            "r.dateOfViolation " +
-                            "FROM person p " +
-                            "JOIN student s ON p.personID = s.personID " +
-                            "JOIN enrollment e ON s.studentID = e.studentID " +
-                            "JOIN record r ON r.enrollmentID = e.enrollmentID " +
-                            "JOIN offense o ON r.offenseID = o.offenseID " +
-                            "WHERE e.studentID = ? " +
-                            "ORDER BY r.dateOfViolation DESC");
+                            "s.studentID, " +
+                            "s.personID, " +
+                            "s.address, " +
+                            "s.studentType, " +
+                            "s.departmentID, " +
+                            "p.lastName, " +
+                            "p.firstName, " +
+                            "p.middleName " +
+                            "FROM student s " +
+                            "JOIN person p ON s.personID = p.personID " +
+                            "WHERE s.studentID = ?");
 
             statement.setString(1, StudentId);
             ResultSet rs = statement.executeQuery();
 
-            while (rs.next()) {
-                Student student = new Student();
-                student.setStudentID(rs.getString("studentID"));
-                student.setFirstName(rs.getString("firstName"));
+            while (rs.next())
+            {
+                student.setStudentId(rs.getString("studentID"));
+                student.setPersonID(rs.getLong("personID"));
+                student.setAddress(rs.getString("address"));
+                student.setStudentType(rs.getString("studentType"));
+                student.setDepartmentId(rs.getString("departmentID"));
                 student.setLastName(rs.getString("lastName"));
-                student.setOffenseType(rs.getString("offense_type"));
-                student.setDateOfViolation(rs.getDate("dateOfViolation"));
-                studentOffenceList.add(student);
+                student.setLastName(rs.getString("firstName"));
+                student.setLastName(rs.getString("middleName"));
             }
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        return studentOffenceList;
+        return student;
+    }
+
+    @Override
+    public Student getStudentByName(String lastName, String firstName, String middleName)
+    {
+        Student student = new Student();
+        try (Connection conn = ConnectionHelper.getConnection())
+        {
+            PreparedStatement statement = conn.prepareStatement(
+                        "SELECT " +
+                            "s.studentID," +
+                            "s.personID," +
+                            "s.address," +
+                            "s.studentType," +
+                            "s.departmentID," +
+                            "p.lastName," +
+                            "p.firstName," +
+                            "p.middleName" +
+                            "FROM student s" +
+                            "JOIN person p ON s.personID = p.personID" +
+                            "WHERE p.lastName = ?" +
+                            "AND p.firstName = ?" +
+                            "AND p.middleName = ?;");
+
+            statement.setString(1, lastName);
+            statement.setString(2, firstName);
+            statement.setString(3, middleName);
+            ResultSet rs = statement.executeQuery();
+
+            while (rs.next())
+            {
+                student.setStudentId(rs.getString("studentID"));
+                student.setPersonID(rs.getLong("personID"));
+                student.setAddress(rs.getString("address"));
+                student.setStudentType(rs.getString("studentType"));
+                student.setDepartmentId(rs.getString("departmentID"));
+                student.setLastName(rs.getString("lastName"));
+                student.setLastName(rs.getString("firstName"));
+                student.setLastName(rs.getString("middleName"));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return student;
+    }
+
+    @Override
+    public ArrayList<Record> getStudentRecord(String studentID)
+    {
+        ArrayList<Record> studentRecord = new ArrayList<>();
+        try (Connection conn = ConnectionHelper.getConnection())
+        {
+            PreparedStatement statement = conn.prepareStatement(
+                            "SELECT" +
+                                "r.recordID," +
+                                "r.enrollmentID," +
+                                "r.employeeID," +
+                                "r.offenseID," +
+                                "r.dateOfViolation," +
+                                "r.actionID," +
+                                "r.dateOfResolution," +
+                                "r.remarks," +
+                                "r.status" +
+                                "FROM record r" +
+                                "JOIN enrollment e ON r.enrollmentID = e.enrollmentID " +
+                                "JOIN student s ON e.studentID = s.studentID " +
+                                "WHERE s.studentID = ?;");
+
+            statement.setString(1, studentID);
+            ResultSet rs = statement.executeQuery();
+
+            while (rs.next())
+            {
+                Record record = new Record();
+                record.setRecordId(rs.getString("studentID"));
+                record.setEnrollmentId(rs.getString("employeeID"));
+                record.setOffenseId(rs.getString("offenseID"));
+                record.setDateOfViolation(rs.getDate("dateOfViolation"));
+                record.setActionId(rs.getString("actionID"));
+                record.setDateOfResolution(rs.getDate("dateOfResolution"));
+                record.setRemarks(rs.getString("remarks"));
+                record.setStatus(rs.getString("status"));
+                studentRecord.add(record);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return studentRecord;
+    }
+
+    @Override
+    public Offense getStudentOffense(String offenseID)
+    {
+        Offense offense = new Offense();
+        try (Connection conn = ConnectionHelper.getConnection())
+        {
+            PreparedStatement statement = conn.prepareStatement(
+                    "SELECT " +
+                            "o.offenseID," +
+                            "o.offense," +
+                            "o.type," +
+                            "o.description" +
+                            "FROM offense o where offenseID = ?;");
+
+            statement.setString(1, offenseID);
+            ResultSet rs = statement.executeQuery();
+
+            while (rs.next())
+            {
+                offense.setOffenseId(rs.getString("offenseID"));
+                offense.setOffense(rs.getString("offense"));
+                offense.setType(rs.getString("type"));
+                offense.setDescription(rs.getString("description"));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return offense;
     }
 
     @Override
