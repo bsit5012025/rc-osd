c696afdf7a366db04aebe874762ba0cab5c11184
carlcain <carljustinecain@gmail.com>
2026-02-13 18:25:27 +0800

    OSDA-80: created a unit test and separate DateOfViolation and DateOfResolution queries

--- src/main/java/org/rocs/osd/data/dao/offense/impl/OffenseDaoImpl.java
+++ src/main/java/org/rocs/osd/data/dao/offense/impl/OffenseDaoImpl.java
@@ -2,18 +2,14 @@ package org.rocs.osd.data.dao.offense.impl;
 
 import org.rocs.osd.data.connection.ConnectionHelper;
 import org.rocs.osd.data.dao.offense.OffenseDao;
-import org.rocs.osd.model.offense.Offense;
-import org.rocs.osd.model.person.student.Student;
-import org.rocs.osd.model.record.Record;
 
 import java.sql.*;
-import java.util.ArrayList;
 
 public class OffenseDaoImpl implements OffenseDao
 {
     @Override
     public boolean editExistingViolation(String recordID, String status, String OffenseID,
-                                         String remarks, Date dateOfResolution)
+                                         String remarks)
     {
         try (Connection con = ConnectionHelper.getConnection())
         {
@@ -21,15 +17,13 @@ public class OffenseDaoImpl implements OffenseDao
                     "UPDATE record SET " +
                             "status = ?, " +
                             "offenseID = ?, " +
-                            "remarks = ?, " +
-                            "DateOfResolution = ? " +
+                            "remarks = ? " +
                             "WHERE recordID = ?");
 
             stmt.setString(1, status);
             stmt.setString(2, OffenseID);
             stmt.setString(3, remarks);
-            stmt.setDate(4, dateOfResolution);
-            stmt.setString(5, recordID);
+            stmt.setString(4, recordID);
             stmt.executeUpdate();
 
             return true;
@@ -43,7 +37,8 @@ public class OffenseDaoImpl implements OffenseDao
     }
 
     @Override
-    public boolean editExistingDateOfViolation(String recordID, Date dateOfViolation) {
+    public boolean editExistingDateOfViolation(String recordID, Date dateOfViolation)
+    {
         try (Connection con = ConnectionHelper.getConnection())
         {
             PreparedStatement stmt = con.prepareStatement(
@@ -52,6 +47,30 @@ public class OffenseDaoImpl implements OffenseDao
                             "WHERE recordID = ?");
 
             stmt.setDate(1, dateOfViolation);
+            stmt.setString(2, recordID);
+            stmt.executeUpdate();
+
+            return true;
+        }
+        catch (SQLException e)
+        {
+            System.out.println("An SQL Exception occurred." + e.getMessage());
+
+            return false;
+        }
+    }
+
+    @Override
+    public boolean editExistingDateOfResolution(String recordID, Date dateOfResolution)
+    {
+        try (Connection con = ConnectionHelper.getConnection())
+        {
+            PreparedStatement stmt = con.prepareStatement(
+                    "UPDATE record SET " +
+                            "DateOfResolution = ? " +
+                            "WHERE recordID = ?");
+
+            stmt.setDate(1, dateOfResolution);
             stmt.setString(2, recordID);
             stmt.executeUpdate();
 
