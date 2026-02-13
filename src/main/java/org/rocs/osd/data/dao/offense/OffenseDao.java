87db6b50f60cc33b5b3c5748786802b7cf1301a9
carlcain <carljustinecain@gmail.com>
2026-02-13 16:19:11 +0800

    created Dao logic for updating record

--- src/main/java/org/rocs/osd/data/dao/offense/OffenseDao.java
+++ src/main/java/org/rocs/osd/data/dao/offense/OffenseDao.java
@@ -4,12 +4,13 @@ import org.rocs.osd.model.offense.Offense;
 import org.rocs.osd.model.record.Record;
 import org.rocs.osd.model.person.student.Student;
 
+import java.sql.Date;
 import java.util.ArrayList;
 
 public interface OffenseDao
 {
     boolean editExistingViolation(String recordID, String status, String OffenseID,
-                                  String remarks, String DateOfResolution);
+                                  String remarks, Date DateOfResolution);
 
-    boolean editExistingDateOfViolation(String recordID, String DateOfResolution);
+    boolean editExistingDateOfViolation(String recordID, Date dateOfViolation);
 }
