a2c168a0c95c7f19569372f3228ad477a0965b7a
carlcain <carljustinecain@gmail.com>
2026-02-13 16:19:11 +0800

    created Dao logic for updating record

--- src/main/java/org/rocs/osd/data/dao/offense/impl/OffenseDaoImpl.java
+++ src/main/java/org/rocs/osd/data/dao/offense/impl/OffenseDaoImpl.java
@@ -6,21 +6,62 @@ import org.rocs.osd.model.offense.Offense;
 import org.rocs.osd.model.person.student.Student;
 import org.rocs.osd.model.record.Record;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class OffenseDaoImpl implements OffenseDao
 {
     @Override
-    public boolean editExistingViolation(String recordID, String status, String OffenseID, String remarks, String DateOfResolution) {
-        return false;
+    public boolean editExistingViolation(String recordID, String status, String OffenseID,
+                                         String remarks, Date dateOfResolution)
+    {
+        try (Connection con = ConnectionHelper.getConnection())
+        {
+            PreparedStatement stmt = con.prepareStatement(
+                    "UPDATE record SET " +
+                            "status = ?, " +
+                            "offenseID = ?, " +
+                            "remarks = ?, " +
+                            "DateOfResolution = ? " +
+                            "WHERE recordID = ?");
+
+            stmt.setString(1, status);
+            stmt.setString(2, OffenseID);
+            stmt.setString(3, remarks);
+            stmt.setDate(4, dateOfResolution);
+            stmt.setString(5, recordID);
+            stmt.executeUpdate();
+
+            return true;
+        }
+        catch (SQLException e)
+        {
+            System.out.println("An SQL Exception occurred." + e.getMessage());
+
+            return false;
+        }
     }
 
     @Override
-    public boolean editExistingDateOfViolation(String recordID, String DateOfResolution) {
-        return false;
+    public boolean editExistingDateOfViolation(String recordID, Date dateOfViolation) {
+        try (Connection con = ConnectionHelper.getConnection())
+        {
+            PreparedStatement stmt = con.prepareStatement(
+                    "UPDATE record SET " +
+                            "DateOfViolation = ? " +
+                            "WHERE recordID = ?");
+
+            stmt.setDate(1, dateOfViolation);
+            stmt.setString(2, recordID);
+            stmt.executeUpdate();
+
+            return true;
+        }
+        catch (SQLException e)
+        {
+            System.out.println("An SQL Exception occurred." + e.getMessage());
+
+            return false;
+        }
     }
 }
