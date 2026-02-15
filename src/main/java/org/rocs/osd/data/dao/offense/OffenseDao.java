ce515a7ef9eca8c1f4901898b3d55c6b5a8914ad
carlcain <carljustinecain@gmail.com>
2026-02-15 15:18:23 +0800

    OSDA-109: change Long value to long

--- src/main/java/org/rocs/osd/data/dao/offense/OffenseDao.java
+++ src/main/java/org/rocs/osd/data/dao/offense/OffenseDao.java
@@ -10,12 +10,12 @@ import java.util.ArrayList;
 public interface OffenseDao
 {
     ArrayList<Student>getAllStudent();
-    ArrayList<Student>getStudentByDepartmentID(Long departmentID);
+    ArrayList<Student>getStudentByDepartmentID(long departmentID);
     Student getStudentById(String studentID);
     Student getStudentByName(String lastName, String firstName, String middleName);
     ArrayList<Record> getStudentRecord(String studentID);
     Offense getStudentOffense(String offenseID);
-    boolean addStudentViolation(Long enrollmentID, String employeeID,
-                                Long offenseID, Date dateOfViolation, Long actionID,
+    boolean addStudentViolation(long enrollmentID, String employeeID,
+                                long offenseID, Date dateOfViolation, long actionID,
                                 String remarks, String status);
 }
