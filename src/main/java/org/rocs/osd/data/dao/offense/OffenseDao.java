f1402d5f33f145f3c3f248d940a2f53aa306e417
carlcain <carljustinecain@gmail.com>
2026-02-15 20:06:53 +0800

    OSDA-109: made some changes

--- src/main/java/org/rocs/osd/data/dao/offense/OffenseDao.java
+++ src/main/java/org/rocs/osd/data/dao/offense/OffenseDao.java
@@ -5,15 +5,15 @@ import org.rocs.osd.model.record.Record;
 import org.rocs.osd.model.person.student.Student;
 
 import java.sql.Date;
-import java.util.ArrayList;
+import java.util.List;
 
 public interface OffenseDao
 {
-    ArrayList<Student>getAllStudent();
-    ArrayList<Student>getStudentByDepartmentID(long departmentID);
+    List<Student> getAllStudent();
+    List<Student>getStudentByDepartmentID(long departmentID);
     Student getStudentById(String studentID);
     Student getStudentByName(String lastName, String firstName, String middleName);
-    ArrayList<Record> getStudentRecord(String studentID);
+    List<Record> getStudentRecord(String studentID);
     Offense getStudentOffense(String offenseID);
     boolean addStudentViolation(long enrollmentID, String employeeID,
                                 long offenseID, Date dateOfViolation, long actionID,
