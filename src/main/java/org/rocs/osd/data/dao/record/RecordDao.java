caf91ccd02870d7150baf5acfde78344d4298c9f
carlcain <carljustinecain@gmail.com>
2026-02-16 21:58:48 +0800

    OSDA-109: Create Dao for Recording Violation (#21)
    * creat dao with unit test
    
    * OSDA-109: Made some changes on the DAO Query and modify test to accomidate the changes
    
    * OSDA-109: change void addStudentViolation to boolean addStudentViolation, fix the student package structure and made some changes on the query
    
    * creat dao with unit test
    
    # Conflicts:
    #	src/main/java/org/rocs/osd/data/dao/offense/OffenseDao.java
    #	src/main/java/org/rocs/osd/data/dao/offense/impl/OffenseDaoImpl.java
    #	src/test/java/org/rocs/osd/data/dao/offense/impl/OffenseDaoImplTest.java
    
    * OSDA-109: Made some changes on the DAO Query and modify test to accomidate the changes
    
    * OSDA-109: change void addStudentViolation to boolean addStudentViolation, fix the student package structure and made some changes on the query
    
    * OSDA-109: added the models that zen created and made a few changes on my DAO for recording violation
    
    * OSDA-109: created test for added methods
    
    * OSDA-109: remove semicolons on my query
    
    * OSDA-109: change some String datatype to Date
    
    * OSDA-109: change the code to be compatible with the latest update from master branch
    
    * OSDA-109: created two method for filtering departments
    
    * OSDA-109: remove unecessary file(RC_BG.png)
    
    * OSDA-109: change Long value to long
    
    * OSDA-109: tried to
    
    * OSDA-109: made some changes
    
    * OSDA-109: remove methods about student
    
    * OSDA-109: Created record, student, and offence package. Modify methods name and get student record either by school year, student level or all record if the student level and school year is null
    
    * OSDA-109: Changes for the test method name
    
    * OSDA-109: applied changes

---
+++ src/main/java/org/rocs/osd/data/dao/record/RecordDao.java
@@ -0,0 +1,15 @@
+package org.rocs.osd.data.dao.record;
+
+import org.rocs.osd.model.person.student.Student;
+import org.rocs.osd.model.record.Record;
+
+import java.sql.Date;
+import java.util.List;
+
+public interface RecordDao
+{
+    List<Record> findStudentByIdAndEnrolment(String studentID, String schoolYear, String studentLevel);
+    boolean addStudentRecord(long enrollmentID, String employeeID,
+                             long offenseID, Date dateOfViolation, long actionID,
+                             String remarks, String status);
+}
---
+++ src/test/java/org/rocs/osd/data/dao/offense/impl/OffenseDaoImplTest.java
@@ -0,0 +1,79 @@
+package org.rocs.osd.data.dao.offense.impl;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.rocs.osd.data.connection.ConnectionHelper;
+
+import org.rocs.osd.data.dao.offense.OffenseDao;
+import org.rocs.osd.model.offense.Offense;
+import org.rocs.osd.model.person.student.Student;
+import org.rocs.osd.model.record.Record;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class OffenseDaoImplTest
+{
+    @Mock
+    private Connection connection;
+
+    @Mock
+    private PreparedStatement preparedStatement;
+
+    @Mock
+    private ResultSet resultSet;
+
+    private static MockedStatic<ConnectionHelper> connectionHelper;
+
+    @BeforeEach
+    public void setUp() throws SQLException {
+        connectionHelper = Mockito.mockStatic(ConnectionHelper.class);
+        connectionHelper.when(ConnectionHelper::getConnection).thenReturn(connection);
+        when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        connectionHelper.close();
+    }
+
+    @Test
+    void testFindOffenseById() throws SQLException
+    {
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(true).thenReturn(false);
+
+        when(resultSet.getLong("offenseID")).thenReturn(Long.valueOf(1));
+        when(resultSet.getString("offense")).thenReturn("Vaping");
+        when(resultSet.getString("type")).thenReturn("Major Offense");
+        when(resultSet.getString("description")).thenReturn("Bringing vape");
+
+        OffenseDao dao = new OffenseDaoImpl();
+        Offense offense = dao.findOffenseById("OFF-001");
+
+        assertEquals(1, offense.getOffenseId());
+        assertEquals("Vaping", offense.getOffense());
+        assertEquals("Major Offense", offense.getType());
+        assertEquals("Bringing vape", offense.getDescription());
+
+        verify(connection, times(1)).prepareStatement(anyString());
+        verify(preparedStatement, times(1)).setString(1, "OFF-001");
+        verify(preparedStatement, times(1)).executeQuery();
+    }
+}
