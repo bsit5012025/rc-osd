ac0d85890ca9dcc5e98ff2ce9266bacf668866f2
Keith Jasper Quevada <keithjasper.quevada@gmail.com>
2026-02-24 21:43:00 +0800

    add controller offense to navigate add offense modal

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,7 @@ module org.rocs.osd {
 
     opens org.rocs.osd.controller.login to javafx.fxml;
     opens org.rocs.osd.controller.dashboard to javafx.fxml;
+    opens org.rocs.osd.controller.offense to javafx.fxml;
 
     exports org.rocs.osd;
 }
