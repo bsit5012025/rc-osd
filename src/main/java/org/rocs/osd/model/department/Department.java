c593f0ddaace6f6d34667f5e1f76e525550aba2f
carlcain <carljustinecain@gmail.com>
2026-03-01 16:20:44 +0800

    OSDA-80: try to revert model to master

--- src/main/java/org/rocs/osd/model/department/Department.java
+++ src/main/java/org/rocs/osd/model/department/Department.java
@@ -1,12 +1,24 @@
 package org.rocs.osd.model.department;
 
+/**
+ * Represents a Department entity.
+ * This class contains the department ID and department name.
+ */
 public class Department {
     private long departmentId;
     private String departmentName;
 
+    /**
+     * Default constructor.
+     */
     public Department() {
     }
 
+    /**
+     * Constructor to create a Department with ID and name.
+     * @param departmentId   the ID of the department.
+     * @param departmentName the name of the department.
+     */
     public Department(long departmentId, String departmentName) {
 
         // Department ID
@@ -15,22 +27,33 @@ public class Department {
         this.departmentName = departmentName;
     }
 
-    // This will get the Department ID
+    /**
+     *  This will get the Department ID
+     *  return the department ID
+     */
+
     public long getDepartmentId() {
         return departmentId;
     }
 
-    // This will set the Department ID
+    /**
+     *   This will set the Department ID
+     */
     public void setDepartmentId(long departmentId) {
         this.departmentId = departmentId;
     }
 
-    // This will get the Department Name
+    /**
+     * This will get the Department Name
+     * return the department ID
+      */
     public String getDepartmentName() {
         return departmentName;
     }
 
-    // This will set the Department Name
+    /**
+     * This will set the Department Name
+      */
     public void setDepartmentName(String departmentName) {
         this.departmentName = departmentName;
     }
--- src/main/java/org/rocs/osd/model/enrollment/Enrollment.java
+++ src/main/java/org/rocs/osd/model/enrollment/Enrollment.java
@@ -1,18 +1,41 @@
 package org.rocs.osd.model.enrollment;
 
+/**
+ * Represents Enrollment in the system.
+ * This class stores information about a student's enrollment record.
+ */
 public class Enrollment {
+    /** Unique identifier of the enrollment */
     private long enrollmentId;
+    /** Unique identifier of the student */
     private String studentId;
+    /** Academic school year of the enrollment */
     private String schoolYear;
+    /** Student level */
     private String studentLevel;
+    /** Section of the student */
     private String section;
+    /** Department identifier */
     private String departmentId;
+    /** Disciplinary status identifier */
     private String disciplinaryStatusId;
 
+    /**
+     * Default constructor.
+     */
     public Enrollment() {
     }
 
-    // Constructor with all values
+    /**
+     * Constructs an Enrollment with all required fields.
+     * @param enrollmentId the unique enrollment ID.
+     * @param studentId the student ID.
+     * @param schoolYear the school year.
+     * @param studentLevel the student level.
+     * @param section the assigned section.
+     * @param departmentId the department ID.
+     * @param disciplinaryStatusId  the disciplinary status ID.
+     */
     public Enrollment(long enrollmentId, String studentId, String schoolYear, String studentLevel, String section, String departmentId, String disciplinaryStatusId) {
         this.enrollmentId = enrollmentId;
         this.studentId = studentId;
@@ -23,72 +46,113 @@ public class Enrollment {
         this.disciplinaryStatusId = disciplinaryStatusId;
     }
 
-    //This will get the Enrollment ID
+    /**
+     * This will get the Enrollment ID
+     * @return the enrollment ID
+     */
+
     public long getEnrollmentId() {
         return enrollmentId;
     }
 
-    //This will set the Enrollment ID
+    /**
+     * This will set the Enrollment ID
+     */
+
     public void setEnrollmentId(long enrollmentId) {
         this.enrollmentId = enrollmentId;
     }
 
-    //This will get the Student ID
+    /**
+     *  This will get the Student ID
+     *   @return the student ID
+     */
+
     public String getStudentId() {
         return studentId;
     }
 
-    //This will set the Student ID
+    /**
+     * This will set the Student ID
+     */
+
     public void setStudentId(String studentId) {
         this.studentId = studentId;
     }
 
-    //This will get the School Year
+    /**
+     *  This will get the School Year
+     *  @return the school year
+     */
     public String getSchoolYear() {
         return schoolYear;
     }
 
-    //This will set the School Year
+    /**
+     *  This will set the School Year
+     */
     public void setSchoolYear(String schoolYear) {
         this.schoolYear = schoolYear;
     }
 
-    //This will get the Student Level
+    /**
+     *  This will get the Student Level
+     *  @return the student level
+     */
     public String getStudentLevel() {
         return studentLevel;
     }
 
-    //This will set the Student Level
+    /**
+     *   This will set the Student Level
+     */
+
     public void setStudentLevel(String studentLevel) {
         this.studentLevel = studentLevel;
     }
 
-    //This will get the Section
+    /**
+     * This will get the Section
+     *  @return the section
+     */
     public String getSection() {
         return section;
     }
 
-    //This will set the Section
+    /**
+     *   This will set the Section
+     */
+
     public void setSection(String section) {
         this.section = section;
     }
 
-    //This will get the Department ID
+    /**
+     *   This will get the Department ID
+     * @return the department ID
+     */
     public String getDepartmentId() {
         return departmentId;
     }
 
-    //This will set the Department ID
+    /**
+     * This will set the Department ID
+     */
     public void setDepartmentId(String departmentId) {
         this.departmentId = departmentId;
     }
 
-    //This will get the Disciplinary Status ID
+    /**
+     *  This will get the Disciplinary Status ID
+     * @return the Disciplinary Status ID
+     */
     public String getDisciplinaryStatusId() {
         return disciplinaryStatusId;
     }
 
-    //This will set the Disciplinary Status ID
+    /**
+     * This will set the Disciplinary Status ID
+     */
     public void setDisciplinaryStatusId(String disciplinaryStatusId) {
         this.disciplinaryStatusId = disciplinaryStatusId;
     }
