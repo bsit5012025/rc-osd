d2d9bd01f87059f2ae981256d0fcc45cda67191a
“[wilroww]” <wilroww.reosa.bayona@gmail.com>
2026-03-02 04:44:40 +0800

    Change AppealFacade base on Dao

---
+++ src/main/java/org/rocs/osd/facade/appeal/impl/AppealFacadeImpl.java
@@ -0,0 +1,28 @@
+package org.rocs.osd.facade.appeal.impl;
+
+import org.rocs.osd.data.dao.appeal.AppealDao;
+import org.rocs.osd.data.dao.appeal.impl.AppealDaoImpl;
+import org.rocs.osd.facade.appeal.AppealFacade;
+import org.rocs.osd.model.appeal.Appeal;
+
+import java.util.List;
+
+public class AppealFacadeImpl implements AppealFacade {
+
+    private final AppealDao appealDao = new AppealDaoImpl();
+
+    @Override
+    public List<Appeal> getAllAppeals() {
+        return appealDao.findAllAppealDetails();
+    }
+
+    @Override
+    public void approveAppeal(Long appealId) {
+        appealDao.updateAppealStatus(appealId, "APPROVED");
+    }
+
+    @Override
+    public void rejectAppeal(Long appealId) {
+        appealDao.updateAppealStatus(appealId, "REJECTED");
+    }
+}
