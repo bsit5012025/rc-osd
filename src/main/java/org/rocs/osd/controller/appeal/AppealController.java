96601fa36cd7781d0942b0e9de2f8af723a11178
wilroww <wilroww.reosa.bayona@gmail.com>
2026-03-04 11:00:46 +0800

    Create AppealCOntroller and its modal for connection on appealModal.fxml later (#64)
    Co-authored-by: “[wilroww]” <“wilroww.reosa.bayona@gmail.com“>

---
+++ src/main/java/org/rocs/osd/controller/appeal/AppealController.java
@@ -0,0 +1,48 @@
+package org.rocs.osd.controller.appeal;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.VBox;
+import org.rocs.osd.facade.appeal.AppealFacade;
+import org.rocs.osd.facade.appeal.impl.AppealFacadeImpl;
+import org.rocs.osd.model.appeal.Appeal;
+
+import java.io.IOException;
+import java.util.List;
+
+public class AppealController {
+
+    @FXML
+    private VBox listContainer;
+
+    private AppealFacade appealFacade = new AppealFacadeImpl();
+
+    @FXML
+    public void initialize() {
+
+        listContainer.getChildren().clear();
+        loadAppealsFromDB();
+    }
+
+    private void loadAppealsFromDB() {
+
+        List<Appeal> appeals = appealFacade.getAllAppeals();
+
+        for (Appeal appeal : appeals) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/appeal/appealModal.fxml"));
+
+                VBox card = loader.load();
+
+                AppealModalController controller = loader.getController();
+                controller.setAppeal(appeal);
+
+                controller.setOnActionComplete(() -> listContainer.getChildren().remove(card)
+                );
+                listContainer.getChildren().add(card);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
