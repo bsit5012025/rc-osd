8b72a5a7a8f4ce1cb139597298a87bdbfb699868
carlcain <carljustinecain@gmail.com>
2026-03-13 19:42:44 +0800

    OSDA-271: update login dao to fetch data from person table (#88)
    * OSDA-270: modified Login model to get Person object
    
    * OSDA-271: modified findLoginByUsername method to get Person model
    
    * OSDA-271: modified test for findLoginByUsername method
    
    * OSDA-271: improve query for personID and login id

--- src/main/java/org/rocs/osd/data/dao/login/impl/LoginDaoImpl.java
+++ src/main/java/org/rocs/osd/data/dao/login/impl/LoginDaoImpl.java
@@ -3,6 +3,7 @@ package org.rocs.osd.data.dao.login.impl;
 import org.rocs.osd.data.connection.ConnectionHelper;
 import org.rocs.osd.data.dao.login.LoginDao;
 import org.rocs.osd.model.login.Login;
+import org.rocs.osd.model.person.Person;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -27,14 +28,33 @@ public class LoginDaoImpl implements LoginDao  {
         Login login = new Login();
 
         try (Connection conn = ConnectionHelper.getConnection()) {
-            PreparedStatement statement = conn.prepareStatement("SELECT * FROM LOGIN WHERE username = ?");
+            PreparedStatement statement = conn.prepareStatement(
+                    "SELECT " +
+                            "l.id, " +
+                            "l.username, " +
+                            "l.password, " +
+                            "l.personID, "+
+                            "p.lastname, " +
+                            "p.firstname, " +
+                            "p.middleName " +
+                            "from Login l " +
+                            "JOIN person p on l.personID = p.personID " +
+                            "WHERE l.username = ?");
+
             statement.setString(1, username);
             ResultSet rs = statement.executeQuery();
 
-            while (rs.next()) {
+            if(rs.next()) {
+                Person person = new Person();
+                person.setPersonID(rs.getLong("personID"));
+                person.setLastName(rs.getString("lastname"));
+                person.setFirstName(rs.getString("firstname"));
+                person.setMiddleName(rs.getString("middleName"));
+
                 login.setId(rs.getLong("id"));
                 login.setUsername(rs.getString("username"));
                 login.setPassword(rs.getString("password"));
+                login.setPerson(person);
             }
 
         } catch (SQLException e) {
