fd3b3512da0f413325fd644b1cfff955eb163854
carlcain <carljustinecain@gmail.com>
2026-03-15 22:26:38 +0800

    OSDA-173: Added all Test methods

--- src/test/java/org/rocs/osd/facade/request/impl/RequestFacadeImplTest.java
+++ src/test/java/org/rocs/osd/facade/request/impl/RequestFacadeImplTest.java
@@ -8,8 +8,11 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.rocs.osd.data.dao.request.RequestDao;
 import org.rocs.osd.facade.request.RequestFacade;
 import org.rocs.osd.model.request.Request;
+import org.rocs.osd.model.request.RequestStatus;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
@@ -18,8 +21,6 @@ class RequestFacadeImplTest
 {
     @Mock
     private RequestDao requestDao;
-
-    private Request request;
 
     private RequestFacade requestFacade;
 
@@ -31,7 +32,7 @@ class RequestFacadeImplTest
     @Test
     void testAddRequest()
     {
-        when(requestDao.addRequest(anyString(),anyString(),anyString(),anyString())).thenReturn(true);
+        when(requestDao.addRequest(anyString(),anyString(),nullable(String.class),anyString())).thenReturn(true);
 
         boolean status = requestFacade.addRequest(
                 "EMP-001",
@@ -41,7 +42,6 @@ class RequestFacadeImplTest
         );
 
         assertTrue(status);
-
         verify(requestDao, times(1)).addRequest(
                 "EMP-001",
                 "St. Andrew",
@@ -53,12 +53,39 @@ class RequestFacadeImplTest
     @Test
     void testGetAllRequest()
     {
+        Request request = new Request();
+        request.setRequestID(1L);
+        request.setEmployeeID("EMP-001");
+        request.setDetails("St. Andrew");
+        request.setType("By Section");
+        request.setMessage("Requesting for the conduct record of all students in St. Andrew");
+        request.setStatus(RequestStatus.APPROVED);
 
+        when(requestDao.findAllRequests()).thenReturn(List.of(request));
+
+        List<Request> list = requestFacade.getAllRequest();
+        Request list1 = list.get(0);
+
+        assertNotNull(list);
+        assertFalse(list.isEmpty());
+        assertEquals(1L ,list1.getRequestID());
+        assertEquals("EMP-001" ,list1.getEmployeeID());
+        assertEquals("St. Andrew" ,list1.getDetails());
+        assertEquals("By Section" ,list1.getType());
+        assertEquals("Requesting for the conduct record of all students in St. Andrew" ,list1.getMessage());
+        assertEquals(RequestStatus.APPROVED ,list1.getStatus());
+
+        verify(requestDao, times(1)).findAllRequests();
     }
 
     @Test
     void testUpdateRequestStatus()
     {
+        when(requestDao.updateRequestStatus(anyLong(), any(RequestStatus.class))).thenReturn(true);
 
+        boolean status = requestFacade.updateRequestStatus(1L, RequestStatus.APPROVED);
+
+        assertTrue(status);
+        verify(requestDao, times(1)).updateRequestStatus(1L, RequestStatus.APPROVED);
     }
 }
