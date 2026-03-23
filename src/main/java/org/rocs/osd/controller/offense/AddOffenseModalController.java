c6755c3671a1461f8070aca4413d91ad388215a0
juswa <markjoshua.camama@gmail.com>
2026-03-24 01:24:44 +0800

    OSDA-275: "Improved" getting the IP for the SMS gateway android application and fixed checkstyles

--- src/main/java/org/rocs/osd/controller/offense/AddOffenseModalController.java
+++ src/main/java/org/rocs/osd/controller/offense/AddOffenseModalController.java
@@ -5,6 +5,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
@@ -15,14 +16,12 @@ import org.rocs.osd.data.dao.disciplinaryAction.DisciplinaryActionDao;
 import org.rocs.osd.data.dao.disciplinaryAction.impl.DisciplinaryActionImpl;
 import org.rocs.osd.data.dao.enrollment.EnrollmentDao;
 import org.rocs.osd.data.dao.enrollment.impl.EnrollmentDaoImpl;
-import org.rocs.osd.data.dao.guardian.impl.GuardianDaoImpl;
 import org.rocs.osd.data.dao.offense.OffenseDao;
 import org.rocs.osd.data.dao.offense.impl.OffenseDaoImpl;
 import org.rocs.osd.data.dao.record.RecordDao;
 import org.rocs.osd.data.dao.record.impl.RecordDaoImpl;
 import org.rocs.osd.data.dao.student.StudendDao;
 import org.rocs.osd.data.dao.student.impl.StudentDaoImpl;
-import org.rocs.osd.facade.guardian.GuardianFacade;
 import org.rocs.osd.facade.guardian.impl.GuardianFacadeImpl;
 import org.rocs.osd.facade.record.RecordFacade;
 import org.rocs.osd.facade.record.impl.RecordFacadeImpl;
@@ -30,16 +29,17 @@ import org.rocs.osd.model.offense.Offense;
 import org.rocs.osd.model.person.guardian.Guardian;
 import org.rocs.osd.model.person.student.Student;
 import org.rocs.osd.model.person.studentGuardian.StudentGuardian;
+import org.rocs.osd.facade.guardian.GuardianFacade;
+import static org.rocs.osd.controller.sms.SmsService.formatPhone;
 
-import java.awt.*;
 import java.sql.Date;
 
-import static org.rocs.osd.controller.sms.SmsService.formatPhone;
+
 
 /**
  * Controller for the "Add Offense" modal in the Office of
  * Student Discipline System.
- *
+ *<p>
  *  This class handles the population of offense type
  * and level ComboBoxes and automatically selects the
  * level of offense based on the selected offense type.
@@ -49,31 +49,41 @@ public class AddOffenseModalController {
     /**
      * Dropdown for selecting offense type.
      */
-    @FXML private ComboBox<String> offenseTypeComboBox;
+    @FXML
+    private ComboBox<String> offenseTypeComboBox;
     /**
      * Dropdown for displaying offense level.
      */
-    @FXML private ComboBox<String> levelOfOffenseComboBox;
+    @FXML
+    private ComboBox<String> levelOfOffenseComboBox;
     /**
      * Dropdown for selecting disciplinary action.
      */
-    @FXML private ComboBox<String> actionComboBox;
+    @FXML
+    private ComboBox<String> actionComboBox;
     /**
      * Input field for student ID.
      */
-    @FXML private TextField studentIdTextField;
+    @FXML
+    private TextField studentIdTextField;
     /**
      * Input field for student name.
      */
-    @FXML private TextField studentNameTextField;
+    @FXML
+    private TextField studentNameTextField;
     /**
      * Date picker for selecting violation date.
      */
-    @FXML private DatePicker datePicker;
+    @FXML
+    private DatePicker datePicker;
     /**
      * Text area for remarks input.
      */
-    @FXML private TextArea remarksTextArea;
+    @FXML
+    private TextArea remarksTextArea;
+    /***/
+    @FXML
+    private CheckBox notifyParentsCheckBox;
     /**
      * DAO for student operations.
      */
@@ -93,8 +103,8 @@ public class AddOffenseModalController {
      * DAO for enrollment operations.
      */
     private EnrollmentDao enrollmentDao;
+    /***/
     private GuardianFacade guardianFacade;
-
     /**
      * Controller for the "Add Offense" modal.
      * Handles the population of offense type and level ComboBoxes and
@@ -108,8 +118,7 @@ public class AddOffenseModalController {
         disciplinaryActionDao = new DisciplinaryActionImpl();
         recordFacade = new RecordFacadeImpl(dao);
         enrollmentDao = new EnrollmentDaoImpl();
-        guardianFacade = new GuardianFacadeImpl(new GuardianDaoImpl());
-
+        guardianFacade = new GuardianFacadeImpl();
         loadComboBoxData();
         autoSelectLevelOfOffense();
         studentIdTextField.setOnAction(e -> autoDisplayStudentName());
@@ -124,9 +133,9 @@ public class AddOffenseModalController {
 
         try {
             ObservableList<String> offenseList = FXCollections.
-            observableArrayList(offenseDao.findAllOffenseName());
+                    observableArrayList(offenseDao.findAllOffenseName());
             ObservableList<String> actionList = FXCollections.
-            observableArrayList(disciplinaryActionDao.findAllAction());
+                    observableArrayList(disciplinaryActionDao.findAllAction());
             offenseTypeComboBox.setItems(offenseList);
             actionComboBox.setItems(actionList);
         } catch (Exception e) {
@@ -135,7 +144,6 @@ public class AddOffenseModalController {
         }
 
     }
-
     /**
      * Automatically selects the level of offense
      * based on the offense type chosen by the user.
@@ -209,22 +217,24 @@ public class AddOffenseModalController {
             String remarks = remarksTextArea.getText();
 
 
-            if (studentId.isEmpty() || studentName.isEmpty()
-                    || offenseType == null || actionName == null
+            if (studentId.isEmpty()
+                    || studentName.isEmpty()
+                    || offenseType == null
+                    || actionName == null
                     || datePicker.getValue() == null) {
-
                 System.out.println("Fill out missing fields!");
                 return;
             }
+
 
             Date dateOfViolation = java.sql.Date.valueOf(datePicker.getValue());
             String employeeId = "EMP-002";
             Offense offense = offenseDao.findByName(offenseType);
             long offenseId = offense.getOffenseId();
             long actionID = disciplinaryActionDao.
-            findActionIdByName(actionName);
+                    findActionIdByName(actionName);
             long enrollmentId = enrollmentDao.
-            findEnrollmentIdByStudentId(studentId);
+                    findEnrollmentIdByStudentId(studentId);
 
             boolean record = recordFacade.createStudentRecord(
                     enrollmentId,
@@ -236,41 +246,58 @@ public class AddOffenseModalController {
             );
             if (record) {
                 System.out.println("Violation recorded!");
-
                 if (notifyParentsCheckBox.isSelected()) {
                     try {
                         var studentGuardians =
-                                guardianFacade.getGuardianByStudentId(studentId);
+                                guardianFacade.getGuardianByStudentId(
+                                        studentId);
 
-                        if (studentGuardians.isEmpty()) {
+                        if (studentGuardians == null
+                                || studentGuardians.isEmpty()) {
                             System.out.println("No guardians found.");
+                            return;
                         }
 
                         for (StudentGuardian sg : studentGuardians) {
 
                             Guardian guardian = sg.getGuardian();
+                            if (guardian == null
+                                    || guardian.getContactNumber() == null) {
+                                continue;
+                            }
+
                             String phone = guardian.getContactNumber();
                             phone = formatPhone(phone);
 
-                            String message = "Good day!\n I am (NAME) Discipline Officer from Rogationist College. This is to inform you that your child (" + studentName +
-                                    ") committed an offense: " + offenseType + ". Please coordinate with us to settle this dispute. Thank you!";
+                            String message = "Good day!\nI am (NAME) "
+                                    + "Discipline Officer from "
+                                    + "Rogationist College. This is to inform "
+                                    + "you that your child "
+                                    + studentName
+                                    + ", committed an offense: \n\n"
+                                    + offenseType
+                                    + "\n \n"
+                                    + "Please coordinate with us thru call or "
+                                    + "in-person to settle this matter. "
+                                    + "Thank you!";
 
                             SmsService.sendSMSAsync(phone, message);
 
-                            System.out.println("SMS sent to: " + phone);
+                            System.out.println("SMS sent to: "
+                                    + phone);
                         }
 
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
                 }
-
-                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                Stage stage = (Stage) ((Node) event
+                        .getSource()).getScene().getWindow();
                 stage.close();
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
-
